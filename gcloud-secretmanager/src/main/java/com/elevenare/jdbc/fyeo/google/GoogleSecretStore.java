3d342880f58abd16bd66dd867734e7d9ef27372f
George McIntosh <george@georgemcintosh.com>
2024-05-27 22:51:11 +0100

    wio

---
+++ gcloud-secretmanager/src/main/java/com/elevenare/jdbc/fyeo/google/GoogleSecretStore.java
@@ -0,0 +1,10 @@
+package com.elevenare.jdbc.fyeo.google;
+
+import com.elevenware.jdbc.fyeo.SecretStore;
+
+public class GoogleSecretStore implements SecretStore {
+    @Override
+    public String resolve(String key) {
+        return null;
+    }
+}
